9bde910d1d9ee5bb73600c41e96c2fa00d109a3a
Gus Narea <gnarea@users.noreply.github.com>
2023-04-25 12:21:29 +0100

    feat: Implement org deletion

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { AuthorityClient } from './lib/client/AuthorityClient.js';
 export type { AuthorizationHeader } from './lib/client/AuthorizationHeader.js';
 
 export * from './lib/commands/orgCreation.js';
+export * from './lib/commands/orgDeletion.js';
