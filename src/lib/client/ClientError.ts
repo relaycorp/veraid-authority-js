fbf44f1690c1ad8b8ae3d210cf0842fd5fd9c5ad
Gus Narea <gnarea@users.noreply.github.com>
2023-08-29 19:16:08 +0200

    feat(ClientError): Expose response status code

--- src/lib/client/ClientError.ts
+++ src/lib/client/ClientError.ts
@@ -3,4 +3,11 @@ import { AuthorityError } from '../AuthorityError.js';
 /**
  * Represents a 4XX response.
  */
-export class ClientError extends AuthorityError {}
+export class ClientError extends AuthorityError {
+  public constructor(
+    message: string,
+    public readonly statusCode: number,
+  ) {
+    super(message);
+  }
+}
