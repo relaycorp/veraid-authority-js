5edcbf87a9935398dff41092fcd2efadc8c8cd61
Gus Narea <gnarea@users.noreply.github.com>
2023-08-30 15:50:41 +0200

    feat: Export `Command` class

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ export type { AuthorizationHeader } from './lib/client/AuthorizationHeader.js';
 export { ClientError } from './lib/client/ClientError.js';
 export { ServerError } from './lib/client/ServerError.js';
 
+export { Command } from './lib/commands/Command.js';
+
 export * from './lib/commands/orgs/orgCreation.js';
 
 export * from './lib/commands/members/memberCreation.js';
